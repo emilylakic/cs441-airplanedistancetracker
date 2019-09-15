1f3de4241fe8580f0de216bd46367f41450b14eb
Emily Lakic <emilylakic@Emilys-MacBook-Pro.local>
2019-09-15 10:36:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/airplanedistance/MySurface.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.airplanedistance;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class MySurface extends SurfaceView {
+
+    public MySurface(Context context) {
+        super(context);
+    }
+
+}
