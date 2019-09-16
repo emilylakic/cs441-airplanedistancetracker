eda73e469f75e924d727696407255987c14ab3e5
emilylakic <emilylakic@gmail.com>
2019-09-16 10:21:29 -0400

    Figuring out how to work with SurfaceView....

--- app/src/main/java/com/example/myapplication/airplanedistance/MainActivity.java
+++ app/src/main/java/com/example/myapplication/airplanedistance/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.myapplication.airplanedistance;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //SurfaceView view = new SurfaceView(this);
+        //setContentView(view);
+        //view.getHolder().addCallback((SurfaceHolder.Callback) this);
         setContentView(R.layout.activity_main);
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/airplanedistance/MySurface.java
+++ app/src/main/java/com/example/myapplication/airplanedistance/MySurface.java
@@ -1,12 +1,29 @@
 package com.example.myapplication.airplanedistance;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class MySurface extends SurfaceView {
+public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder = null;
 
     public MySurface(Context context) {
         super(context);
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
 }
