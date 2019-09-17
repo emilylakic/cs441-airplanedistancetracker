f27b6381c95d8e7688ea6a432457c8fdeb13be47
emilylakic <emilylakic@gmail.com>
2019-09-17 17:41:53 -0400

    Trying to get surface view to work

--- app/src/main/java/com/example/myapplication/airplanedistance/MainActivity.java
+++ app/src/main/java/com/example/myapplication/airplanedistance/MainActivity.java
@@ -1,28 +1,90 @@
 package com.example.myapplication.airplanedistance;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private Button laButton = null;
+    private Button flButton = null;
+    private boolean drawLine = true;
+    private LinearLayout canvasLayout = null;
+    MySurface customSurfaceView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initControls();
+        customSurfaceView = new MySurface(getApplicationContext());
+        customSurfaceView.setOnTouchListener(this);
+        canvasLayout.addView(customSurfaceView);
+
+        laButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawLine = true;
+
+            }
+        });
+        flButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawLine = false;
+
+            }
+        });
     }
 
     private void initControls() {
-
+        if(laButton == null) {
+            laButton = (Button)findViewById(R.id.laButton);
+        }
+        if(flButton == null) {
+            flButton = (Button)findViewById(R.id.flButton);
+        }
+        if(canvasLayout == null)
+        {
+            canvasLayout = (LinearLayout)findViewById(R.id.customViewLayout);
+        }
     }
 
-//User finger touch surfaceview object
+    /* If user finger touch the surfaceview object. */
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return false;
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        // If user touch the custom SurfaceView object.
+        if(view instanceof SurfaceView) {
+            view.invalidate();
+
+            float x = motionEvent.getX();
+
+            float y = motionEvent.getY();
+
+
+            if (drawLine) {
+
+
+            } else {
+
+            }
+
+            // Tell android os the onTouch event has been processed.
+            return true;
+        }else
+        {
+            // Tell android os the onTouch event has not been processed.
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/airplanedistance/MySurface.java
+++ app/src/main/java/com/example/myapplication/airplanedistance/MySurface.java
@@ -1,12 +1,15 @@
 package com.example.myapplication.airplanedistance;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
 
     private SurfaceHolder surfaceHolder = null;
+    private float circleX = 0;
+    private float circleY = 0;
 
     public MySurface(Context context) {
         super(context);
@@ -15,7 +18,7 @@ public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        drawLine();
     }
 
     @Override
@@ -25,8 +28,12 @@ public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+//whatever you use to create the line, like paint = null;
     }
 
-    //public void () { //this method will be invoked to draw a
-}
+    public void  drawLine() {
+        surfaceHolder = getHolder();
+        Canvas canvas = surfaceHolder.lockCanvas();
+
+        }
+    }
